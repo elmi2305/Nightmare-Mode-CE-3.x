543093aa96b7d55fe0cb1a10f58d8c585696fa0f
Elmi <elmir.junuzovic7blm@gmail.com>
2026-04-11 00:50:46 +0200

    changed default EMI effect position to be left (compressed)

--- src/main/java/com/itlesports/nightmaremode/mixin/EmiConfigMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/EmiConfigMixin.java
@@ -1,16 +1,21 @@
 package com.itlesports.nightmaremode.mixin;
 
 import emi.dev.emi.emi.config.ConfigPresets;
+import emi.dev.emi.emi.config.EffectLocation;
 import emi.dev.emi.emi.config.EmiConfig;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(EmiConfig.class)
 public class EmiConfigMixin {
+    @Shadow public static EffectLocation effectLocation;
+
     @Inject(method = "<clinit>", at = @At("TAIL"))
     private static void manageDefaultConfig(CallbackInfo ci){
         ConfigPresets.oldDefault.run();
+        effectLocation = EffectLocation.LEFT_COMPRESSED;
     }
 }
