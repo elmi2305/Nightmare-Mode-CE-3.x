781bcfacaf2c59903b2bbd2207229f580cd54736
Elmi <elmir.junuzovic7blm@gmail.com>
2026-04-28 23:51:53 +0200

    add locator pile entity interface methods

--- src/main/java/com/itlesports/nightmaremode/util/interfaces/LocatorPileEntityExt.java
+++ src/main/java/com/itlesports/nightmaremode/util/interfaces/LocatorPileEntityExt.java
@@ -1,4 +1,7 @@
 package com.itlesports.nightmaremode.util.interfaces;
 
 public interface LocatorPileEntityExt {
+    void nightmareMode$setDenominator(double d);
+    double nightmareMode$getDenominator();
+    void nightmareMode$moveTowards3D(double x, double y, double z);
 }
