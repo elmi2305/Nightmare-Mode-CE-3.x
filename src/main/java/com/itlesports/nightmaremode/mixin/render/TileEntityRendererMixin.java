b7756a8cd3d075ec1f8e869768a4c3867ba8c599
Elmi <elmir.junuzovic7blm@gmail.com>
2026-04-28 23:55:17 +0200

    register blood bone tile entity renderer

--- src/main/java/com/itlesports/nightmaremode/mixin/render/TileEntityRendererMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/render/TileEntityRendererMixin.java
@@ -1,20 +1,18 @@
 package com.itlesports.nightmaremode.mixin.render;
 
+import com.itlesports.nightmaremode.block.tileEntities.TileEntityBloodBone;
 import com.itlesports.nightmaremode.block.tileEntities.TileEntityBloodChest;
 import com.itlesports.nightmaremode.block.tileEntities.TileEntitySteelLocker;
-import com.itlesports.nightmaremode.entity.EntityBloodZombie;
+import com.itlesports.nightmaremode.rendering.TileEntityBloodBoneRenderer;
 import com.itlesports.nightmaremode.rendering.TileEntityBloodChestRenderer;
 import com.itlesports.nightmaremode.rendering.TileEntitySteelLockerRenderer;
-import net.minecraft.src.TileEntity;
 import net.minecraft.src.TileEntityRenderer;
 import net.minecraft.src.TileEntitySpecialRenderer;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Mutable;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.Map;
 
@@ -28,6 +26,7 @@ public abstract class TileEntityRendererMixin {
     private void addCustomNightmareRendering(CallbackInfo ci){
         this.specialRendererMap.put(TileEntityBloodChest.class, new TileEntityBloodChestRenderer());
         this.specialRendererMap.put(TileEntitySteelLocker.class, new TileEntitySteelLockerRenderer());
+        this.specialRendererMap.put(TileEntityBloodBone.class, new TileEntityBloodBoneRenderer());
 
         TileEntityRenderer thisObj = (TileEntityRenderer) (Object) this;
         for (Object renderer : this.specialRendererMap.values()) {
@@ -35,5 +34,6 @@ public abstract class TileEntityRendererMixin {
         }
         ((TileEntitySpecialRenderer) this.specialRendererMap.get(TileEntityBloodChest.class)).setTileEntityRenderer(thisObj);
         ((TileEntitySpecialRenderer) this.specialRendererMap.get(TileEntitySteelLocker.class)).setTileEntityRenderer(thisObj);
+        ((TileEntitySpecialRenderer) this.specialRendererMap.get(TileEntityBloodBone.class)).setTileEntityRenderer(thisObj);
     }
 }
