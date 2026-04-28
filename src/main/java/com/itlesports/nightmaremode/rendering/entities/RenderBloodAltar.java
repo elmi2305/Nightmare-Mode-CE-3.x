cb0a9fa46ab0ba4c0aab3561b7ef36ab36103f3c
Elmi <elmir.junuzovic7blm@gmail.com>
2026-04-28 23:49:37 +0200

    add blood altar entity renderer

--- src/main/java/com/itlesports/nightmaremode/rendering/entities/RenderBloodAltar.java
+++ src/main/java/com/itlesports/nightmaremode/rendering/entities/RenderBloodAltar.java
@@ -1,4 +1,14 @@
 package com.itlesports.nightmaremode.rendering.entities;
 
-public class RenderBloodAltar {
+import net.minecraft.src.*;
+
+public class RenderBloodAltar extends RenderEntity {
+
+
+    @Override
+    public void doRender(Entity entity, double d, double e, double f, float g, float h) {
+        BossStatus.setBossStatus((IBossDisplayData) entity, true);
+        super.doRender(entity, d, e, f, g, h);
+    }
+
 }
