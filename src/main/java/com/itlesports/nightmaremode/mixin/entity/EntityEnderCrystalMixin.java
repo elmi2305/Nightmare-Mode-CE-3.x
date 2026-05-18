f06f6a00919e30f7e5aa120ec6a0edcf4b629f44
Elmi <elmir.junuzovic7blm@gmail.com>
2026-05-18 19:15:45 +0200

    no end-crystal lightning on NoHit

--- src/main/java/com/itlesports/nightmaremode/mixin/entity/EntityEnderCrystalMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/entity/EntityEnderCrystalMixin.java
@@ -1,5 +1,6 @@
 package com.itlesports.nightmaremode.mixin.entity;
 
+import btw.community.nightmaremode.NightmareMode;
 import com.itlesports.nightmaremode.util.NMDifficultyParam;
 import net.minecraft.src.*;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,25 +10,23 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-
 @Mixin(EntityEnderCrystal.class)
 public abstract class EntityEnderCrystalMixin extends Entity{
     public EntityEnderCrystalMixin(World par1World) {
         super(par1World);
     }
-    @Inject(
-            method = "attackEntityFrom",
-            at = @At(value = "INVOKE", target = "Lnet/minecraft/src/EntityEnderCrystal;setDead()V")
-    )
-    // makes ender crystals strike the player with lightning when they are destroyed
-    private void avengeDestroyer(DamageSource par1DamageSource, float par2, CallbackInfoReturnable<Boolean> cir) {
-        Entity destroyer = par1DamageSource.getSourceOfDamage();
+
+    @Inject(method = "attackEntityFrom", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/EntityEnderCrystal;setDead()V"))
+    private void avengeDestroyer(DamageSource src, float par2, CallbackInfoReturnable<Boolean> cir) {
+        Entity destroyer = src.getSourceOfDamage();
+
         if (destroyer instanceof EntityArrow) {
             destroyer = ((EntityArrow) destroyer).shootingEntity;
         } else if (destroyer instanceof EntityThrowable) {
             destroyer = ((EntityThrowable) destroyer).getThrower();
         }
-        if (destroyer instanceof EntityPlayer && this.dimension != 0 && this.worldObj.getDifficultyParameter(NMDifficultyParam.ShouldMobsBeBuffed.class)) {
+
+        if (destroyer instanceof EntityPlayer && this.dimension != 0 && !NightmareMode.noHit && this.worldObj.getDifficultyParameter(NMDifficultyParam.ShouldMobsBeBuffed.class)) {
             Entity lightningbolt = new EntityLightningBolt(this.worldObj, destroyer.posX, destroyer.posY-0.5, destroyer.posZ);
             this.worldObj.addWeatherEffect(lightningbolt);
         }
@@ -48,10 +47,12 @@ public abstract class EntityEnderCrystalMixin extends Entity{
     private void updateEndCrystalSize(World par1World, CallbackInfo ci){
         this.setSize(1.8f,3.2f);
     }
+
     @Inject(method = "onUpdate", at = @At("TAIL"))
     private void manageDespawnIfDeloaded(CallbackInfo ci){
         if(this.dimension != 1 && this.ridingEntity == null && this.ticksExisted >= 3000){this.setDead();}
     }
+
     @Inject(method = "attackEntityFrom", at = @At("HEAD"),cancellable = true)
     private void removeChainExplosions(DamageSource par1DamageSource, float par2, CallbackInfoReturnable<Boolean> cir){
         if(par1DamageSource.isExplosion()){
