f8e04539170990e7e27db1e3bbd6601ce1c2c5ee
Elmi <elmir.junuzovic7blm@gmail.com>
2026-05-19 18:53:28 +0200

    added WorldStateCommand

---
+++ src/main/java/com/itlesports/nightmaremode/util/command/WorldStateCommand.java
@@ -0,0 +1,57 @@
+package com.itlesports.nightmaremode.util.command;
+
+import btw.community.nightmaremode.NightmareMode;
+import btw.world.BTWWorldData;
+import net.minecraft.src.*;
+
+import java.util.List;
+
+public class WorldStateCommand extends CommandBase {
+    @Override
+    public String getCommandName() {
+        return "state";
+    }
+
+    public String getCommandUsage(ICommandSender iCommandSender) {
+        return "commands.state.usage";
+    }
+
+    public List getCommandAliases() {
+        return List.of("ws", "state", "worldstate", "settst", "st");
+    }
+
+    @Override
+    public int getRequiredPermissionLevel() {
+        return 2;
+    }
+
+    @Override
+    public void processCommand(ICommandSender sender, String[] args) throws CommandException {
+        EntityPlayerMP player = (EntityPlayerMP) sender;
+
+        if (args.length == 0) {
+            player.sendChatToPlayer((new ChatMessageComponent()).addText("World state is " + NightmareMode.worldState));
+            return;
+        }
+        int choice;
+        try {
+            choice = Integer.parseInt(args[0]);
+        } catch (NumberFormatException e) {
+            throw new WrongUsageException("/ws <0|1|2|3>");
+        }
+        NightmareMode.worldState = choice;
+
+        boolean netherAccessed = choice >= 1;
+        boolean witherSummoned = choice >= 2;
+        boolean endAccessed = choice >= 3;
+
+        player.worldObj.setData(BTWWorldData.NETHER_ACCESSED, netherAccessed);
+        player.worldObj.setData(BTWWorldData.WITHER_SUMMONED, witherSummoned);
+        player.worldObj.setData(BTWWorldData.END_ACCESSED, endAccessed);
+        player.worldObj.setData(BTWWorldData.DRAGON_KILLED, endAccessed);
+        player.worldObj.setData(NightmareMode.DRAGON_DEFEATED, endAccessed);
+
+        NightmareMode.sendWorldStateToAllPlayers();
+        player.sendChatToPlayer((new ChatMessageComponent()).addText("Set world state to " + NightmareMode.worldState));
+    }
+}
