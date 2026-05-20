cfede2f90a91b44d41cbd456a426cf3e5d950354
Elmi <elmir.junuzovic7blm@gmail.com>
2026-05-20 23:11:22 +0200

    made EntityMushWorm spawn when breaking mushroom blocks

--- src/main/java/com/itlesports/nightmaremode/mixin/blocks/MushroomCapBlockMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/blocks/MushroomCapBlockMixin.java
@@ -2,6 +2,10 @@ package com.itlesports.nightmaremode.mixin.blocks;
 
 import btw.block.blocks.MushroomCapBlock;
 import btw.item.BTWItems;
+import com.itlesports.nightmaremode.entity.EntityMushWorm;
+import net.minecraft.src.BlockMushroomCap;
+import net.minecraft.src.Material;
+import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
@@ -12,8 +16,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 import java.util.Random;
 
 @Mixin(MushroomCapBlock.class)
-public class MushroomCapBlockMixin {
+public class MushroomCapBlockMixin extends BlockMushroomCap {
     @Shadow @Final protected int mushroomType;
+
+    public MushroomCapBlockMixin(int i, Material material, int j) {
+        super(i, material, j);
+    }
 
     @Inject(method = "idDropped", at = @At("HEAD"),cancellable = true)
     private void changeIDDropped(int iMetadata, Random rand, int iFortuneModifier, CallbackInfoReturnable<Integer> cir){
@@ -22,5 +30,16 @@ public class MushroomCapBlockMixin {
             return;
         }
         cir.setReturnValue(0);
+    }
+
+    @Override
+    public void breakBlock(World w, int x, int y, int z, int side, int meta) {
+
+        if (w.rand.nextInt(4) == 0 && !w.isRemote) {
+            EntityMushWorm worm = new EntityMushWorm(w);
+            worm.setPositionAndUpdate(x,y,z);
+            w.spawnEntityInWorld(worm);
+        }
+        super.breakBlock(w, x, y, z, side, meta);
     }
 }
