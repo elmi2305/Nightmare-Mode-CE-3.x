3acc9a2d3c0c089d9d014a8741567d7b5c718a22
Elmi <elmir.junuzovic7blm@gmail.com>
2026-05-21 07:34:09 +0200

    removed unnecessary clay imports

--- src/main/java/com/itlesports/nightmaremode/mixin/blocks/UnfiredBrickTileEntityMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/blocks/UnfiredBrickTileEntityMixin.java
@@ -6,13 +6,10 @@ import com.itlesports.nightmaremode.util.NMUtils;
 import net.minecraft.src.TileEntity;
 import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.*;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(UnfiredBrickTileEntity.class)
 public class UnfiredBrickTileEntityMixin extends TileEntity {
-    @Shadow private boolean isCooking;
 
     @ModifyConstant(method = "updateCooking", constant = @Constant(intValue = 11900),remap = false)
     private int reduceClayCookTime(int constant){
