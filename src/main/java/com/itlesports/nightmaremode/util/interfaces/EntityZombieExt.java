3f3a3bb1a07c42c22a0c26e8dcdbdc0ca76f287e
Elmi <elmir.junuzovic7blm@gmail.com>
2026-05-21 05:30:26 +0200

    added EntityZombieExt interface

---
+++ src/main/java/com/itlesports/nightmaremode/util/interfaces/EntityZombieExt.java
@@ -0,0 +1,8 @@
+package com.itlesports.nightmaremode.util.interfaces;
+
+public interface EntityZombieExt {
+    int nightmareMode$getLungedAgo();
+    void nightmareMode$setLungedAgo(int lungedAgo);
+    int nightmareMode$getTimeSpentBreaking();
+    void nightmareMode$setTimeSpentBreaking(int timeSpentBreaking);
+}
