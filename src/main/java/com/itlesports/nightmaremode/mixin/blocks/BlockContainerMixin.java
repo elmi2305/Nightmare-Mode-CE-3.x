dafdecf5f8a47dfed61db619e8ae8bb7d417aff5
Elmi <elmir.junuzovic7blm@gmail.com>
2026-05-30 17:22:35 +0200

    added container logging

---
+++ src/main/java/com/itlesports/nightmaremode/mixin/blocks/BlockContainerMixin.java
@@ -0,0 +1,29 @@
+package com.itlesports.nightmaremode.mixin.blocks;
+
+import btw.community.nightmaremode.NightmareMode;
+import com.itlesports.nightmaremode.util.LogSettings;
+import net.minecraft.src.*;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockContainer.class)
+public class BlockContainerMixin extends Block {
+    protected BlockContainerMixin(int par1, Material par2Material) {
+        super(par1, par2Material);
+    }
+
+    @Inject(method = "breakBlock", at = @At("HEAD"))
+    private void doLogging(World w, int x, int y, int z, int m, int par6, CallbackInfo ci) {
+        if(NightmareMode.getInstance().isGriefLogging() && !w.isRemote){
+            LogSettings ls = NightmareMode.getInstance().getLogSettings();
+            if(!ls.logIndirectBreaks) return;
+            String text = this.getLocalizedName() + " destroyed at " + x + " " + y + " " + z + ". Nearest Player: " + w.getClosestPlayer(x,y,z, 32).username;
+
+            if(ls.logContainers){
+                NightmareMode.appendLogLine(text);
+            }
+        }
+    }
+}
