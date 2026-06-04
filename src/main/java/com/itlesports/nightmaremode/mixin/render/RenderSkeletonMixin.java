fde2be926ba5a477ec42292412f7fad3288a50af
Elmi <elmir.junuzovic7blm@gmail.com>
2026-06-04 23:15:38 +0200

    added Lightning Skeleton render

--- src/main/java/com/itlesports/nightmaremode/mixin/render/RenderSkeletonMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/render/RenderSkeletonMixin.java
@@ -1,10 +1,10 @@
 package com.itlesports.nightmaremode.mixin.render;
 
+import com.itlesports.nightmaremode.entity.creepers.EntityCreeperVariant;
 import com.itlesports.nightmaremode.util.NMFields;
 import com.itlesports.nightmaremode.util.NMUtils;
-import net.minecraft.src.EntitySkeleton;
-import net.minecraft.src.RenderSkeleton;
-import net.minecraft.src.ResourceLocation;
+import net.minecraft.src.*;
+import org.lwjgl.opengl.GL11;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
@@ -12,7 +12,7 @@ import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RenderSkeleton.class)
-public class RenderSkeletonMixin {
+public class RenderSkeletonMixin extends RenderBiped {
     @Unique private static final ResourceLocation FIRE = new ResourceLocation("nightmare:textures/entity/skeletonFire.png");
     @Unique private static final ResourceLocation ICE = new ResourceLocation("nightmare:textures/entity/skeletonIce.png");
     @Unique private static final ResourceLocation ENDER = new ResourceLocation("nightmare:textures/entity/skeletonEnder.png");
@@ -26,7 +26,51 @@ public class RenderSkeletonMixin {
     @Unique private static final ResourceLocation WITHER_ECLIPSE = new ResourceLocation("nightmare:textures/entity/witherskeletonEclipse.png");
     @Unique private static final ResourceLocation NORMAL_ECLIPSE = new ResourceLocation("nightmare:textures/entity/normalskeletonEclipse.png");
 
+    @Unique private static final ResourceLocation armor = new ResourceLocation("textures/entity/creeper/creeper_armor.png");
 
+    public RenderSkeletonMixin(ModelBiped par1ModelBiped, float par2) {
+        super(par1ModelBiped, par2);
+    }
+
+    @Override
+    protected int shouldRenderPass(EntityLivingBase entity, int par2, float par3) {
+        if (((EntitySkeleton)entity).getSkeletonType().id() == NMFields.SKELETON_LIGHTNING) {
+            return this.renderSkeletonPassModel((EntitySkeleton) entity, par2, par3);
+        }
+
+        return super.shouldRenderPass(entity, par2, par3);
+    }
+
+
+    @Unique protected int renderSkeletonPassModel(EntitySkeleton c, int par2, float par3) {
+        GL11.glDepthMask(!c.isInvisible());
+
+        if (par2 == 1) {
+            float var4 = (float)c.ticksExisted + par3;
+            this.bindTexture(armor);
+            GL11.glMatrixMode(5890);
+            GL11.glLoadIdentity();
+            float var5 = var4 * 0.01f;
+            float var6 = var4 * 0.01f;
+            GL11.glTranslatef(var5, var6, 0.0f);
+            this.setRenderPassModel(this.mainModel);
+            GL11.glMatrixMode(5888);
+            GL11.glEnable(3042);
+            float var7 = 0.5f;
+            GL11.glColor4f(var7, var7, var7, 1.0f);
+            GL11.glDisable(2896);
+            GL11.glBlendFunc(1, 1);
+            return 1;
+        }
+        if (par2 == 2) {
+            GL11.glMatrixMode(5890);
+            GL11.glLoadIdentity();
+            GL11.glMatrixMode(5888);
+            GL11.glEnable(2896);
+            GL11.glDisable(3042);
+        }
+        return -1;
+    }
 
     @Inject(method = "func_110860_a", at = @At("HEAD"), cancellable = true)
     private void manageVariantTextures(EntitySkeleton skeleton, CallbackInfoReturnable<ResourceLocation> cir) {
