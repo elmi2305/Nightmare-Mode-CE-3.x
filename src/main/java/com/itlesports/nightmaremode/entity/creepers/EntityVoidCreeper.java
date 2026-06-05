601b8a4f25bdacfb3182cf781cc5377793ac0abb
Elmi <elmir.junuzovic7blm@gmail.com>
2026-06-05 14:01:16 +0200

    tuned void creeper

--- src/main/java/com/itlesports/nightmaremode/entity/creepers/EntityVoidCreeper.java
+++ src/main/java/com/itlesports/nightmaremode/entity/creepers/EntityVoidCreeper.java
@@ -3,6 +3,7 @@ package com.itlesports.nightmaremode.entity.creepers;
 import btw.community.nightmaremode.NightmareMode;
 import com.itlesports.nightmaremode.entity.underworld.EntityBlackHole;
 import com.itlesports.nightmaremode.util.NMFields;
+import com.itlesports.nightmaremode.util.NMUtils;
 import net.minecraft.src.World;
 
 public class EntityVoidCreeper extends EntityCreeperVariant{
@@ -12,13 +13,13 @@ public class EntityVoidCreeper extends EntityCreeperVariant{
         this.soundPitchModifier = -0.5f;
         this.fuseTime = 50;
         this.canLunge = false;
-        this.explosionRadius = 1;
-        this.explosionMultiplier = 1f;
+        this.explosionRadius = 0;
+        this.explosionMultiplier = 0f;
     }
 
     @Override
     protected void onDeathEffect() {
-        this.worldObj.spawnEntityInWorld(new EntityBlackHole(this.worldObj, this.posX, this.posY, this.posZ, 2));
+        this.worldObj.spawnEntityInWorld(new EntityBlackHole(this.worldObj, this.posX, this.posY, this.posZ, 1 + ((double) NMUtils.getWorldProgress() / 3)));
         super.onDeathEffect();
     }
     @Override
