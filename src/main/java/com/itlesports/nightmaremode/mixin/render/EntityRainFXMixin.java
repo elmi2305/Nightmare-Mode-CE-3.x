28a58f51e265ef62a7c04d15b7ffb78b85b625bc
Elmi <elmir.junuzovic7blm@gmail.com>
2026-06-07 17:26:53 +0200

    colored rain particles for portal opening

--- src/main/java/com/itlesports/nightmaremode/mixin/render/EntityRainFXMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/render/EntityRainFXMixin.java
@@ -1,9 +1,7 @@
 package com.itlesports.nightmaremode.mixin.render;
 
 import com.itlesports.nightmaremode.util.NMUtils;
-import net.minecraft.src.EntityFX;
-import net.minecraft.src.EntityRainFX;
-import net.minecraft.src.World;
+import net.minecraft.src.*;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -16,8 +14,9 @@ public class EntityRainFXMixin extends EntityFX {
     }
 
     @Inject(method = "<init>", at = @At("TAIL"))
-    private void manageBloodRain(World par1World, double par2, double par4, double par6, CallbackInfo ci){
-        if (NMUtils.getIsBloodMoon()) {
+    private void manageBloodRain(World w, double par2, double par4, double par6, CallbackInfo ci){
+        EntityPlayer p = Minecraft.getMinecraft().thePlayer;
+        if (NMUtils.getIsBloodMoon() || NMUtils.isNearActiveRitual(p, 128)) {
             this.particleRed = 1.0f;
             this.particleGreen = 0.05f;
             this.particleBlue = 0.05f;
