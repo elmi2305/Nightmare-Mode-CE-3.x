7ed57190f9fb9dccc0fc999c9ce1f5569b2fbcbf
Elmi <elmir.junuzovic7blm@gmail.com>
2026-06-08 19:25:33 +0200

    added eclipse items as a tag

--- src/main/java/com/itlesports/nightmaremode/item/NMTags.java
+++ src/main/java/com/itlesports/nightmaremode/item/NMTags.java
@@ -8,6 +8,27 @@ import net.minecraft.src.ResourceLocation;
 
 public class NMTags {
     public static final Tag sandwichMeats = Tag.of(loc("sandwich_meats"), Item.beefCooked, Item.chickenCooked, Item.porkCooked, BTWItems.cookedMutton, BTWItems.cookedCheval, BTWItems.cookedWolfChop, BTWItems.cookedMysteryMeat, BTWItems.hamAndEggs);
+    public static final Tag eclipseDrops = Tag.of(loc("eclipse_drops"),
+            NMItems.darksunFragment,
+            NMItems.magicFeather,
+            NMItems.bloodMilk,
+            NMItems.creeperChop,
+            NMItems.voidSack,
+            NMItems.charredFlesh,
+            NMItems.spiderFangs,
+            NMItems.fireRod,
+            NMItems.waterRod,
+            NMItems.sulfur,
+            NMItems.creeperTear,
+            NMItems.silverLump,
+            NMItems.witheredBone,
+            NMItems.voidMembrane,
+            NMItems.decayedFlesh,
+            NMItems.ghastTentacle,
+            NMItems.elementalRod,
+            NMItems.shadowRod,
+            NMItems.speedCoil
+    );
 
     private static ResourceLocation loc(String name) {
         return new ResourceLocation("nightmare", name);
