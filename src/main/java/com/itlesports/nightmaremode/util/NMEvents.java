fadd8e88b31dc93987747b1cc61bd5b9156ebdf6
Elmi <elmir.junuzovic7blm@gmail.com>
2026-06-08 21:39:32 +0200

    refactored NMEvents and added a few new ones

--- src/main/java/com/itlesports/nightmaremode/util/NMEvents.java
+++ src/main/java/com/itlesports/nightmaremode/util/NMEvents.java
@@ -6,63 +6,108 @@ import net.minecraft.src.*;
 public class NMEvents {
 
     public enum SimpleEvent {
-        SLIME_RAIN("nmSlimeRain", ConditionType.DAY_ENDS_WITH, 33, 1, NMFields.HARDMODE) {
-            @Override protected void doCustomEventLogic(World w) {
+        SLIME_RAIN("nmSlimeRain", ConditionType.DAY_ENDS_WITH, 33, 1, NMFields.HARDMODE, EnumChatFormatting.GREEN) {
+            @Override
+            protected void doCustomEventLogic(World w) {
                 w.setRainStrength(1.0f);
                 w.getWorldInfo().setRaining(true);
                 w.getWorldInfo().setRainTime(12000);
-                if(w.getWorldInfo().isThundering()){
+                if (w.getWorldInfo().isThundering()) {
                     w.getWorldInfo().setThundering(false);
                 }
-            }};
+            }
+        },
+
+        JELLY_SLIMES(ConditionType.DAY_ENDS_WITH, 76, 2, NMFields.PREHARDMODE) {
+            @Override
+            protected void doCustomEventLogic(World w) {}
+        },
+
+        GREAT_HARVEST("nmGreatHarvest", ConditionType.DAY_MULTIPLE, 44, 3, NMFields.HARDMODE, EnumChatFormatting.GOLD) {
+            @Override
+            protected void doCustomEventLogic(World w) {
+                if (w.isRaining()) {
+                    w.getWorldInfo().setRaining(false);
+                }
+            }
+        };
 
         private final String string;
         private final ConditionType condition;
+        private final int constant;                    // ← moved here
         private boolean wasChosen;
         public final int id;
         public final int worldStateRequirement;
+        public EnumChatFormatting color = EnumChatFormatting.WHITE;
 
-        SimpleEvent(String name1, ConditionType conditionType, int constant, int id, int worldStateRequirement){
+        // Main constructor
+        SimpleEvent(String name1, ConditionType conditionType, int constant, int id,
+                    int worldStateRequirement, EnumChatFormatting color) {
             this.string = name1;
             this.condition = conditionType;
-            this.condition.setConstantNumber(constant);
+            this.constant = constant;
             this.id = id;
             this.worldStateRequirement = worldStateRequirement;
+            this.color = color;
         }
 
-        public boolean isActive(){
+        SimpleEvent(String name1, ConditionType conditionType, int constant, int id,
+                    int worldStateRequirement) {
+            this(name1, conditionType, constant, id, worldStateRequirement, EnumChatFormatting.WHITE);
+        }
+
+        SimpleEvent(ConditionType conditionType, int constant, int id,
+                    int worldStateRequirement, EnumChatFormatting color) {
+            this(null, conditionType, constant, id, worldStateRequirement, color);
+        }
+
+        SimpleEvent(ConditionType conditionType, int constant, int id,
+                    int worldStateRequirement) {
+            this(null, conditionType, constant, id, worldStateRequirement, EnumChatFormatting.WHITE);
+        }
+
+        public boolean isActive() {
             return NightmareMode.getInstance().activeEventsInt == this.id;
-        };
-        public enum ConditionType{
-            DAY_MULTIPLE() {
-                @Override protected boolean checkActivation(World w) {
-                    return (w.isDaytime() || NMUtils.getIsDayFromWorldTime(w)) && NMUtils.getDayCountFromWorld(w) % this.constantNumber == 0;
-                }},
-            DAY_ENDS_WITH() {
-                @Override protected boolean checkActivation(World w) {
+        }
+
+        public enum ConditionType {
+            DAY_MULTIPLE {
+                @Override
+                protected boolean checkActivation(World w, int constant) {
+                    return (w.isDaytime() || NMUtils.getIsDayFromWorldTime(w))
+                            && NMUtils.getDayCountFromWorld(w) % constant == 0;
+                }
+            },
+            DAY_ENDS_WITH {
+                @Override
+                protected boolean checkActivation(World w, int constant) {
+                    // first day check goes off of the skylight...
                     String dayCount = Integer.toString(NMUtils.getDayCountFromWorld(w));
-                    // first day check goes off of the skylight, which is set to low during an eclipse and thus the check would fail
-                    return (w.isDaytime() || NMUtils.getIsDayFromWorldTime(w)) && dayCount.endsWith(Integer.toString(this.constantNumber));
-                }},
-            NIGHT_MULTIPLE() {
-                @Override protected boolean checkActivation(World w) {
-                    return !(w.isDaytime() || NMUtils.getIsDayFromWorldTime(w)) && NMUtils.getDayCountFromWorld(w) % this.constantNumber == 0;
-
-                }},
-            NIGHT_ENDS_WITH() {
-                @Override protected boolean checkActivation(World w) {
+                    return (w.isDaytime() || NMUtils.getIsDayFromWorldTime(w))
+                            && dayCount.endsWith(Integer.toString(constant));
+                }
+            },
+            NIGHT_MULTIPLE {
+                @Override
+                protected boolean checkActivation(World w, int constant) {
+                    return !(w.isDaytime() || NMUtils.getIsDayFromWorldTime(w))
+                            && NMUtils.getDayCountFromWorld(w) % constant == 0;
+                }
+            },
+            NIGHT_ENDS_WITH {
+                @Override
+                protected boolean checkActivation(World w, int constant) {
                     String dayCount = Integer.toString(NMUtils.getDayCountFromWorld(w));
+                    return !(w.isDaytime() || NMUtils.getIsDayFromWorldTime(w))
+                            && dayCount.endsWith(Integer.toString(constant));
+                }
+            };
 
-                    return !(w.isDaytime() || NMUtils.getIsDayFromWorldTime(w)) && dayCount.endsWith(Integer.toString(this.constantNumber));
-                }};
+            protected abstract boolean checkActivation(World w, int constant);
+        }
 
-            int constantNumber;
-
-            ConditionType(){}
-            public void setConstantNumber(int constantNumber){
-                this.constantNumber = constantNumber;
-            }
-            protected abstract boolean checkActivation(World w);
+        public boolean shouldActivate(World w) {
+            return condition.checkActivation(w, this.constant);
         }
 
         protected abstract void doCustomEventLogic(World w);
@@ -71,13 +116,20 @@ public class NMEvents {
             if(this.wasChosen) return;
             // this helps it only run initialization logic once!
             this.doCustomEventLogic(w);
+            if(this.string == null) return; // event does not want to be displayed in chat
 
             ChatMessageComponent text = new ChatMessageComponent();
+            ChatMessageComponent text1 = new ChatMessageComponent();
             text.addText(I18n.getString("nm.events." + this.string));
-            text.setColor(EnumChatFormatting.WHITE);
+            text.setColor(this.color);
+            text.setBold(true);
+
+            text1.addText(I18n.getString("nm.events." + this.string + ".info"));
+            text1.setColor(EnumChatFormatting.WHITE);
             for(Object p : w.playerEntities){
 
                 ((EntityPlayer)p).sendChatToPlayer(text);
+                ((EntityPlayer)p).sendChatToPlayer(text1);
             }
         }
     }
@@ -86,7 +138,7 @@ public class NMEvents {
         SimpleEvent chosen = null;
 
         for (SimpleEvent event : SimpleEvent.values()) {
-            if (chosen == null && event.condition.checkActivation(w) && NMUtils.getWorldProgress() >= event.worldStateRequirement) {
+            if (chosen == null && event.condition.checkActivation(w, event.constant) && NMUtils.getWorldProgress() >= event.worldStateRequirement) {
                 chosen = event;
                 event.displayEventAndStart(w);
                 event.wasChosen = true;
