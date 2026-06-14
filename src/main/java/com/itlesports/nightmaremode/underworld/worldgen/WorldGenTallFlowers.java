b4cba2c9a9f46edea3722007ed314c6dffa8f654
Elmi <elmir.junuzovic7blm@gmail.com>
2026-06-14 22:46:21 +0200

    tweaked flower generation & fixed top block

--- src/main/java/com/itlesports/nightmaremode/underworld/worldgen/WorldGenTallFlowers.java
+++ src/main/java/com/itlesports/nightmaremode/underworld/worldgen/WorldGenTallFlowers.java
@@ -30,14 +30,14 @@ public class WorldGenTallFlowers extends WorldGenerator {
 
             if (!w.isAirBlock(nx, ny, nz)) continue;
             // roof guard relevant for nether-style dim. never passes in the underworld. still here for future reference
-            if (w.provider.hasNoSky && ny >= 127) continue;
+            if (w.provider.hasNoSky && ny >= 255) continue;
             // ground adjacency and block-specific placement rules (checks the block below)
             if (!this.plantBlock.canBlockStayDuringGenerate(w, nx, ny, nz)) continue;
 
             int type = this.exactType ? this.types : rand.nextInt(this.types);
             w.setBlock(nx, ny, nz, this.plantBlockId, type, 2);
             // sunflower often needs top half
-            if (type == BlockTallFlower.SUNFLOWER || rand.nextInt(6) != 0) {
+            if (type == BlockTallFlower.SUNFLOWER || rand.nextInt(6) != 0 && type != BlockTallFlower.LAVAFLOWER) {
                 w.setBlock(nx, ny + 1, nz, this.plantBlockId, type | 0x8, 2);
             }
         }
