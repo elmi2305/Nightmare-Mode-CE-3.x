a5aff5e677bebb58020767b8798912962c8f554d
Elmi <elmir.junuzovic7blm@gmail.com>
2026-06-26 06:47:48 +0200

    added NMDevUtils

---
+++ src/main/java/com/itlesports/nightmaremode/util/NMDevUtils.java
@@ -0,0 +1,9 @@
+package com.itlesports.nightmaremode.util;
+
+import btw.community.nightmaremode.NightmareMode;
+
+public class NMDevUtils {
+    public static boolean getDevMode(){
+        return false;
+    }
+}
