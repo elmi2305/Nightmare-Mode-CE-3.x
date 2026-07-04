ff67d3465d6fc6a6d25728be019d1bf42553785b
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-04 11:55:14 +0200

    cleaned up EnumCreatureTypeMixin

--- src/main/java/com/itlesports/nightmaremode/mixin/EnumCreatureTypeMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/EnumCreatureTypeMixin.java
@@ -1,6 +1,7 @@
 package com.itlesports.nightmaremode.mixin;
 
 import btw.community.nightmaremode.NightmareMode;
+import com.itlesports.nightmaremode.util.NMFields;
 import net.minecraft.src.EnumCreatureType;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -18,8 +19,11 @@ public class EnumCreatureTypeMixin {
     @Inject(method = "<init>", at = @At("TAIL"))
     private void changeMobCap(CallbackInfo ci){
         if(this.maxNumberOfCreature == 90){
-            this.maxNumberOfCreature = (NightmareMode.worldState == 0 ? 100 : (NightmareMode.worldState == 1 ? 110 : (NightmareMode.worldState == 2 ? 120 : 130)));
-                                                        // it used to be 105                 ->                 115                                -> 130-> 140
+            this.maxNumberOfCreature = (
+                    NightmareMode.worldState == NMFields.PREHARDMODE ? 100 :
+                            (NightmareMode.worldState == NMFields.HARDMODE ? 110 :
+                             (NightmareMode.worldState == NMFields.POSTWITHER ? 120 : 130)));
+            // it used to be 105 -> 115 -> 130 -> 140
         }
     }
 }
