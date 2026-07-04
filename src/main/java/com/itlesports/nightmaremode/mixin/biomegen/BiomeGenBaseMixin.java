baac7d0b4179fdc41caf0681d7044b0008117377
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-04 13:49:35 +0200

    removed faulty BiomeGenBaseMixin

--- src/main/java/com/itlesports/nightmaremode/mixin/biomegen/BiomeGenBaseMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/biomegen/BiomeGenBaseMixin.java
@@ -1,40 +1,10 @@
 package com.itlesports.nightmaremode.mixin.biomegen;
 
-import com.itlesports.nightmaremode.entity.variants.EntityRainSpider;
-import com.itlesports.nightmaremode.util.elements.NMEvents;
 import net.minecraft.src.*;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-import java.util.List;
 
 @Mixin(BiomeGenBase.class)
 public class BiomeGenBaseMixin {
-    @Shadow protected List spawnableMonsterList;
-    @Shadow protected List spawnableWaterCreatureList;
-
-
-    @Inject(method = "getSpawnableList", at = @At("HEAD"), cancellable = true)
-    private void spiders(EnumCreatureType type, CallbackInfoReturnable<List> cir){
-        if(type == EnumCreatureType.monster){
-            if(NMEvents.SimpleEvent.SPIDER_RAIN.isActive()){
-                List temp = this.spawnableMonsterList;
-                temp.add(new SpawnListEntry(EntityRainSpider.class, 100, 1, 4));
-                cir.setReturnValue(temp);
-                return;
-            }
-
-            if(NMEvents.SimpleEvent.HELL.isActive()){
-                List temp = this.spawnableMonsterList;
-                temp.add(new SpawnListEntry(EntityPigZombie.class, 3, 1, 1));
-                temp.add(new SpawnListEntry(EntityGhast.class, 1, 1, 1));
-                temp.add(new SpawnListEntry(EntityMagmaCube.class, 2, 1, 1));
-                cir.setReturnValue(temp);
-                return;
-            }
-        }
-    }
+    // do not mixin into getSpawnableList or any method like that. it runs once per chunk load. you will create bugs that are basically undetectable :D
 }
