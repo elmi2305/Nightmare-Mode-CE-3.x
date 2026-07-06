90953dfb31855eefefe320346dfe929275aae798
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-06 19:20:17 +0200

    add zombie pigman texture for hell event

--- src/main/java/com/itlesports/nightmaremode/mixin/render/RenderZombieMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/render/RenderZombieMixin.java
@@ -2,6 +2,7 @@ package com.itlesports.nightmaremode.mixin.render;
 
 import com.itlesports.nightmaremode.util.NMUtils;
 import com.itlesports.nightmaremode.entity.variants.EntityStoneZombie;
+import com.itlesports.nightmaremode.util.elements.NMEvents;
 import net.minecraft.src.EntityPigZombie;
 import net.minecraft.src.EntityZombie;
 import net.minecraft.src.RenderZombie;
@@ -14,19 +15,27 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(RenderZombie.class)
 public class RenderZombieMixin {
-    @Unique private static final ResourceLocation ZOMBIE_TEXTURE_ECLIPSE = new ResourceLocation("nightmare:textures/entity/zombieEclipseHigh.png");
-    @Unique private static final ResourceLocation PIGMAN_TEXTURE_ECLIPSE = new ResourceLocation("nightmare:textures/entity/zombiePigmanEclipse.png");
-    @Unique private static final ResourceLocation ZOMBIE_TEXTURE_STONE = new ResourceLocation("nightmare:textures/entity/zombieStone.png");
+    @Unique private static final ResourceLocation ZOMBIE_ECLIPSE = new ResourceLocation("nightmare:textures/entity/zombieEclipseHigh.png");
+    @Unique private static final ResourceLocation PIG_ECLIPSE = new ResourceLocation("nightmare:textures/entity/zombiePigmanEclipse.png");
+    @Unique private static final ResourceLocation PIGMAN_HELL = new ResourceLocation("nightmare:textures/entity/zombiePigmanHell.png");
+    @Unique private static final ResourceLocation ZOMBIE_STONE = new ResourceLocation("nightmare:textures/entity/zombieStone.png");
 
     @Inject(method = "func_110863_a", at = @At("HEAD"),cancellable = true)
-    private void manageEclipsedTextures(EntityZombie par1EntityZombie, CallbackInfoReturnable<ResourceLocation> cir){
-        if(par1EntityZombie instanceof EntityStoneZombie){
-            cir.setReturnValue(ZOMBIE_TEXTURE_STONE);
-        } else if(NMUtils.getIsMobEclipsed(par1EntityZombie)){
-            if (par1EntityZombie instanceof EntityPigZombie) {
-                cir.setReturnValue(PIGMAN_TEXTURE_ECLIPSE);
+    private void manageEclipsedTextures(EntityZombie zomb, CallbackInfoReturnable<ResourceLocation> cir){
+        if(zomb instanceof EntityStoneZombie){
+            cir.setReturnValue(ZOMBIE_STONE);
+            return;
+        } else if(NMUtils.getIsMobEclipsed(zomb)){
+            if (zomb instanceof EntityPigZombie) {
+                cir.setReturnValue(PIG_ECLIPSE);
             } else{
-                cir.setReturnValue(ZOMBIE_TEXTURE_ECLIPSE);
+                cir.setReturnValue(ZOMBIE_ECLIPSE);
+            }
+            return;
+        }
+        if(NMEvents.SimpleEvent.HELL.isActive()){
+            if (zomb instanceof EntityPigZombie) {
+                cir.setReturnValue(PIGMAN_HELL);
             }
         }
     }
