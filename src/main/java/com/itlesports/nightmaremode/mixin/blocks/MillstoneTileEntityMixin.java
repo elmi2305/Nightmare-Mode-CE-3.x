063d674ee689874cae42f22ebd58ee1d59917da3
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-10 17:42:32 +0200

    millstones give nausea and produce 8x slower

--- src/main/java/com/itlesports/nightmaremode/mixin/blocks/MillstoneTileEntityMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/blocks/MillstoneTileEntityMixin.java
@@ -2,21 +2,21 @@ package com.itlesports.nightmaremode.mixin.blocks;
 
 import btw.block.blocks.MillstoneBlock;
 import btw.block.tileentity.MillstoneTileEntity;
+import net.minecraft.src.Potion;
+import net.minecraft.src.TileEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 import org.spongepowered.asm.mixin.injection.Redirect;
 
 @Mixin(MillstoneTileEntity.class)
-public class MillstoneTileEntityMixin {
-    @Redirect(method = "updateEntity"
-            ,at = @At(value = "INVOKE",
-            target = "Lbtw/block/tileentity/MillstoneTileEntity;checkForNauseateNearbyPlayers(Lbtw/block/blocks/MillstoneBlock;)V"),remap = false)
-    private void doNotApplyNausea(MillstoneTileEntity instance, MillstoneBlock block){}
+public abstract class MillstoneTileEntityMixin extends TileEntity {
+
 
     @ModifyConstant(method = "updateEntity", constant = @Constant(intValue = 200))
     private int fasterMillstones(int constant){
-        return constant / 2;
+        return constant * 8;
     }
 }
