6a20dacfa91956b8733faf0c08d90f2b420fdb56
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-10 18:42:50 +0200

    removed HardcoreSpawnUtilsMixin benefits

--- src/main/java/com/itlesports/nightmaremode/mixin/HardcoreSpawnUtilsMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/HardcoreSpawnUtilsMixin.java
@@ -22,72 +22,11 @@ public abstract class HardcoreSpawnUtilsMixin{
         worldServer = tempServer;
     }
 
-    @Inject(method = "handleHardcoreSpawn", at = @At("TAIL"))
-    private static void handleExperienceLossOnDeath(MinecraftServer server, EntityPlayerMP oldPlayer, EntityPlayerMP newPlayer, CallbackInfo ci){
-        newPlayer.addExperience(ensurePositive(Math.log10(oldPlayer.experienceTotal * 2) * 16));
-        if(newPlayer.experienceLevel > 0){
-            newPlayer.addExperienceLevel(-1);
-        }
-        if(newPlayer.experienceTotal < 0){
-            newPlayer.experienceTotal = 0;
-        }
-    }
-    @Unique private static int ensurePositive(double input){
-        if(input >= 0){
-            return (int) input;
-        }
-        return 0;
-    }
 
-    @ModifyArg(method = "assignNewHardcoreSpawnLocation", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/WorldServer;setWorldTime(J)V"))
-    private static long setNightOnRespawn(long par1){
-        if(NightmareMode.worldState == 2){
-            return par1;
-        }
-        if(worldServer != null){
-            int moonPhase = worldServer.getMoonPhase();
-            return par1
-                    + (worldServer.getDifficultyParameter(NMDifficultyParam.ShouldMobsBeBuffed.class) ? 18000 : 0)
-                    + (moonPhase == 3 ? 24000 : 0);
-        }
-        return par1;
-        // if the world server was somehow null
-    }
 
     @ModifyConstant(method = "handleHardcoreSpawn", constant = @Constant(longValue = 10800L))
     private static long lowerCooldownForRandomSpawning(long constant){
-        return 600L;
-    }
-    @ModifyArg(method = "onSoftRespawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/EntityPlayerMP;setHealth(F)V"))
-    private static float doNotLowerHealth(float par1){
-        return 20f;
-    }
-    @ModifyArg(method = "onSoftRespawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/FoodStats;setFoodLevel(I)V"))
-    private static int doNotLowerFood(int par1){
-        return 60;
+        return 67676767;
     }
 
-    @Redirect(method = "assignNewHardcoreSpawnLocation", at = @At(value = "INVOKE", target = "Ljava/util/ArrayList;contains(Ljava/lang/Object;)Z"))
-    private static boolean excludeJungle(ArrayList list, Object o){
-        if((o.equals(BiomeGenBase.jungle) || o.equals(BiomeGenBase.jungleHills)) && NightmareMode.bloodmare){
-            return false;
-        }
-        if((o.equals(BiomeGenBase.icePlains) || o.equals(BiomeGenBase.iceMountains))){
-            return true;
-        }
-        return list.contains(o);
-    }
-
-    @Redirect(method = "handleHardcoreSpawn", at = @At(value = "INVOKE", target = "Lnet/minecraft/src/EntityPlayerMP;sendChatToPlayer(Lnet/minecraft/src/ChatMessageComponent;)V",ordinal = 0))
-    private static void avoidSendingChatMessage(EntityPlayerMP instance, ChatMessageComponent par1ChatMessageComponent){}
-
-    @Inject(method = "getPlayerSpawnRadius", at = @At("HEAD"),cancellable = true)
-    private static void makePlayerSpawnCloser(World world, CallbackInfoReturnable<Double> cir){
-        cir.setReturnValue(800 * HardcoreSpawnUtils.getWorldTypeRadiusMultiplier(world) * HardcoreSpawnUtils.getGameProgressRadiusMultiplier(world));
-    }
-
-    @Inject(method = "getPlayerSpawnExclusionRadius", at = @At("HEAD"),cancellable = true)
-    private static void makePlayerSpawnCloser0(World world, CallbackInfoReturnable<Double> cir){
-        cir.setReturnValue(400 * HardcoreSpawnUtils.getWorldTypeRadiusMultiplier(world) * HardcoreSpawnUtils.getGameProgressRadiusMultiplier(world));
-    }
 }
