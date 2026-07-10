307c574e65f26f875e2a1f5aae49cdf2d6a972ac
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-10 18:26:31 +0200

    all items burn

--- src/main/java/com/itlesports/nightmaremode/mixin/entity/EntityItemMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/entity/EntityItemMixin.java
@@ -21,38 +21,4 @@ public abstract class EntityItemMixin extends Entity {
         super(par1World);
     }
 
-    @Inject(method = "attackEntityFrom", at = @At("HEAD"),cancellable = true)
-    private void bloodOrbImmunity(DamageSource par1DamageSource, float par2, CallbackInfoReturnable<Boolean> cir){
-        if(this.getEntityItem() != null && !this.getIsItemBurnable(this.getEntityItem())){
-            cir.setReturnValue(false);
-        }
-    }
-    @Unique private static Set<Integer> nonFlammableItems = null;
-
-    @Unique private Set<Integer> getNonFlammableItems() {
-        if(nonFlammableItems != null) return nonFlammableItems;
-        nonFlammableItems = new HashSet<>(Arrays.asList(
-                NMItems.bloodOrb.itemID,
-                Item.netherStar.itemID,
-                NMItems.starOfTheBloodGod.itemID,
-                Item.blazeRod.itemID,
-                Item.blazePowder.itemID,
-                Block.obsidian.blockID,
-                NMItems.obsidianShard.itemID
-        ));
-        return nonFlammableItems;
-    }
-
-    @Unique
-    private boolean getIsItemBurnable(ItemStack item){
-        if(item == null) return false;
-
-        if(getNonFlammableItems().contains(item.itemID)) return false;
-
-        if(item.getItem() instanceof ArcaneScrollItem) return false;
-
-        if(item.getItem() instanceof NMItem && ((NMItem) item.getItem()).isIndestructible()) return false;
-
-        return true;
-    }
 }
