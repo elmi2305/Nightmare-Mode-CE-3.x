34f84a1bf25abbfc0c06c0a3837fbd49efe96f49
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-10 18:39:38 +0200

    increased steel weight

--- src/main/java/com/itlesports/nightmaremode/mixin/BTWItemsMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/BTWItemsMixin.java
@@ -15,29 +15,10 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(BTWItems.class)
 public class BTWItemsMixin {
-    @Shadow public static Item firePlough;
-    @Shadow public static Item bowDrill;
-
     @Shadow public static Item carrot;
     @Shadow public static Item cookedCarrot;
     @Shadow public static Item boiledPotato;
 
-    @ModifyArg(method = "instantiateModItems", at = @At(value = "INVOKE", target = "Lbtw/item/items/ArmorItemSteel;<init>(III)V", ordinal = 0), index = 2)
-    private static int steelHelmet(int w){
-        return 1;
-    }
-    @ModifyArg(method = "instantiateModItems", at = @At(value = "INVOKE", target = "Lbtw/item/items/ArmorItemSteel;<init>(III)V", ordinal = 1), index = 2)
-    private static int steelChest(int w){
-        return 4;
-    }
-    @ModifyArg(method = "instantiateModItems", at = @At(value = "INVOKE", target = "Lbtw/item/items/ArmorItemSteel;<init>(III)V", ordinal = 2), index = 2)
-    private static int steelLegs(int w){
-        return 3;
-    }
-    @ModifyArg(method = "instantiateModItems", at = @At(value = "INVOKE", target = "Lbtw/item/items/ArmorItemSteel;<init>(III)V", ordinal = 3), index = 2)
-    private static int steelBoots(int w){
-        return 1;
-    }
 
     @Inject(method = "instantiateModItems", at = @At("TAIL"), remap = false)
     private static void replaceItems(CallbackInfo ci){
