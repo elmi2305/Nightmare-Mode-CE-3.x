697ee22fe26f11afc34250b323f121a26e8f4229
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-10 20:00:37 +0200

    incraesed all cook time

--- src/main/java/com/itlesports/nightmaremode/mixin/blocks/CookingVesselTileEntityMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/blocks/CookingVesselTileEntityMixin.java
@@ -2,22 +2,29 @@ package com.itlesports.nightmaremode.mixin.blocks;
 
 import btw.block.tileentity.CookingVesselTileEntity;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.Constant;
 import org.spongepowered.asm.mixin.injection.ModifyConstant;
 
 @Mixin(CookingVesselTileEntity.class)
 public class CookingVesselTileEntityMixin {
+    @Shadow
+    protected int stokedCooldownCounter;
+
     @ModifyConstant(method = "performNormalFireUpdate", constant = @Constant(intValue = 4350),remap = false)
     private int lowerCookTimeNormal(int constant){
-        return Math.max(constant - 2800, 400);
+        return 24000;
     }
     @ModifyConstant(method = "performStokedFireUpdate", constant = @Constant(intValue = 4350),remap = false)
     private int lowerCookTimeStoked(int constant){
-        return Math.max(constant - 2800, 400);
+        return 48000;
     }
 
     @ModifyConstant(method = "updateEntity", constant = @Constant(intValue = 4350))
     private int lowerCookTimeVisually(int constant){
-        return Math.max(constant - 2800, 400);
+        if(this.stokedCooldownCounter > 0){
+            return 48000;
+        }
+        return 24000;
     }
 }
