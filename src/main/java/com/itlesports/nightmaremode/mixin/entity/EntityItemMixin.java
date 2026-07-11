f3eea70676f4cb9c38c1fdc9f7e5d44bfcdb7727
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-11 23:45:08 +0200

    added EntityItem update hook

--- src/main/java/com/itlesports/nightmaremode/mixin/entity/EntityItemMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/entity/EntityItemMixin.java
@@ -1,6 +1,7 @@
 package com.itlesports.nightmaremode.mixin.entity;
 
 import btw.item.items.ArcaneScrollItem;
+import com.itlesports.nightmaremode.block.NMBlocks;
 import com.itlesports.nightmaremode.item.NMItems;
 import com.itlesports.nightmaremode.item.items.template.NMItem;
 import net.minecraft.src.*;
@@ -9,6 +10,7 @@ import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.*;
@@ -19,6 +21,17 @@ public abstract class EntityItemMixin extends Entity {
 
     public EntityItemMixin(World par1World) {
         super(par1World);
+    }
+
+    @Inject(method = "onUpdate", at = @At("TAIL"))
+    private void doWaterCheck(CallbackInfo ci){
+        if(this.getEntityItem() != null && this.getEntityItem().getItem() instanceof NMItem item){
+            int id = item.itemID;
+            if(id == NMBlocks.blockCrushedIronLayer.blockID){
+
+            }
+
+        }
     }
 
 }
