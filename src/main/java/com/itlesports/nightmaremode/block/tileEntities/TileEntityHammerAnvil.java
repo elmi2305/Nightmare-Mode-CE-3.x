e6aaa4ddb3732bdc197b41001531bed7530907aa
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-16 15:11:40 +0200

    fixed hammer not updating client inventory

--- src/main/java/com/itlesports/nightmaremode/block/tileEntities/TileEntityHammerAnvil.java
+++ src/main/java/com/itlesports/nightmaremode/block/tileEntities/TileEntityHammerAnvil.java
@@ -1,10 +1,20 @@
 package com.itlesports.nightmaremode.block.tileEntities;
 
+import com.itlesports.nightmaremode.crafting.manager.HammerCraftingManager;
+import com.itlesports.nightmaremode.crafting.recipe.types.HammerRecipe;
+import com.itlesports.nightmaremode.item.items.ItemHammer;
+import net.minecraft.src.ChatMessageComponent;
+import net.minecraft.src.EntityItem;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.InventoryPlayer;
+import net.minecraft.src.ItemStack;
 import net.minecraft.src.NBTTagCompound;
 import net.minecraft.src.TileEntity;
 
 public class TileEntityHammerAnvil extends TileEntity {
     private static final int SOUND_INTERVAL_TICKS = 2;
+    private static final int MIN_FOOD_LEVEL = 6;
+    private static final float EXHAUSTION_PER_HIT = 0.2F;
 
     protected int maxUses = -1;
     protected int usesRemaining = -1;
@@ -18,6 +28,49 @@ public class TileEntityHammerAnvil extends TileEntity {
     protected TileEntityHammerAnvil(int maxUses) {
         this.maxUses = maxUses;
         this.usesRemaining = maxUses;
+    }
+
+    public boolean tryStartHammerOperation(EntityPlayer player) {
+        if (player == null || this.worldObj == null || this.worldObj.isRemote) {
+            return false;
+        }
+        if (this.isBusy()) {
+            return false;
+        }
+        if (!player.capabilities.isCreativeMode && player.getFoodStats().getFoodLevel() < MIN_FOOD_LEVEL) {
+            this.sendStatus(player, "You are too hungry to use the anvil.");
+            return false;
+        }
+
+        ItemStack input = player.getHeldItem();
+        HammerRecipe recipe = HammerCraftingManager.instance.getRecipe(input);
+        if (recipe == null) {
+            this.sendStatus(player, "No hammer recipe for held item.");
+            return false;
+        }
+
+        int hammerSlot = this.findUsableHammerSlot(player, recipe);
+        if (hammerSlot < 0) {
+            this.sendStatus(player, "A valid hammer is required.");
+            return false;
+        }
+
+        int hits = recipe.getHitsRequired();
+        if (!this.canSpendHits(hits)) {
+            if (!this.isWaitingToBreak()) {
+                this.sendStatus(player, "The anvil is too worn for this recipe.");
+            }
+            return false;
+        }
+
+        this.consumeInputAndDamageHammer(player, input, recipe.getInput(), hammerSlot, hits);
+        recipe.chargePlayerExperience(player);
+        player.addExhaustion(EXHAUSTION_PER_HIT * hits);
+        this.deliverOutputs(player, recipe);
+        this.spendHits(hits);
+        player.inventoryContainer.detectAndSendChanges();
+        player.inventory.onInventoryChanged();
+        return true;
     }
 
     @Override
@@ -87,6 +140,67 @@ public class TileEntityHammerAnvil extends TileEntity {
         this.worldObj.playAuxSFX(2001, this.xCoord, this.yCoord, this.zCoord,
                 this.worldObj.getBlockId(this.xCoord, this.yCoord, this.zCoord));
         this.worldObj.setBlockToAir(this.xCoord, this.yCoord, this.zCoord);
+    }
+
+    private void consumeInputAndDamageHammer(EntityPlayer player, ItemStack input, ItemStack recipeInput, int hammerSlot, int hits) {
+        if (player.capabilities.isCreativeMode) {
+            return;
+        }
+
+        input.stackSize -= recipeInput.stackSize;
+        if (input.stackSize <= 0) {
+            player.inventory.mainInventory[player.inventory.currentItem] = null;
+        }
+
+        ItemStack hammer = player.inventory.mainInventory[hammerSlot];
+        hammer.damageItem(hits, player);
+        if (hammer.stackSize <= 0) {
+            player.inventory.mainInventory[hammerSlot] = null;
+        }
+    }
+
+    private void deliverOutputs(EntityPlayer player, HammerRecipe recipe) {
+        for (ItemStack output : recipe.getOutput()) {
+            if (output == null) {
+                continue;
+            }
+
+            ItemStack outputCopy = output.copy();
+            player.inventory.addItemStackToInventory(outputCopy);
+            if (outputCopy.stackSize > 0) {
+                this.spawnOutputAboveAnvil(outputCopy);
+            }
+        }
+    }
+
+    private void spawnOutputAboveAnvil(ItemStack stack) {
+        EntityItem entity = new EntityItem(this.worldObj, this.xCoord + 0.5D, this.yCoord + 1.2D, this.zCoord + 0.5D, stack);
+        entity.motionX = 0.0D;
+        entity.motionY = 0.0D;
+        entity.motionZ = 0.0D;
+        this.worldObj.spawnEntityInWorld(entity);
+    }
+
+    private int findUsableHammerSlot(EntityPlayer player, HammerRecipe recipe) {
+        InventoryPlayer inventory = player.inventory;
+        for (int i = 0; i < inventory.mainInventory.length; ++i) {
+            ItemStack stack = inventory.mainInventory[i];
+            if (stack != null && stack.getItem() instanceof ItemHammer && recipe.canPlayerUseHammer(stack, player)) {
+                return i;
+            }
+        }
+        return -1;
+    }
+
+    private void sendStatus(EntityPlayer player, String message) {
+        if (player == null || player.worldObj.isRemote) {
+            return;
+        }
+        String suffix = "";
+        if (this.isLimited()) {
+            suffix = " Uses: " + this.getUsesRemaining() + " / " + this.getMaxUses() + ".";
+        }
+        player.sendChatToPlayer(new ChatMessageComponent().addText(message + suffix));
     }
 
     private void queueHammerSounds(int hits) {
--- src/main/java/com/itlesports/nightmaremode/util/HammerAnvilHelper.java
+++ src/main/java/com/itlesports/nightmaremode/util/HammerAnvilHelper.java
@@ -1,113 +1,11 @@
 package com.itlesports.nightmaremode.util;
 
 import com.itlesports.nightmaremode.block.tileEntities.TileEntityHammerAnvil;
-import com.itlesports.nightmaremode.crafting.manager.HammerCraftingManager;
-import com.itlesports.nightmaremode.crafting.recipe.types.HammerRecipe;
-import com.itlesports.nightmaremode.item.items.ItemHammer;
-import net.minecraft.src.ChatMessageComponent;
-import net.minecraft.src.EntityItem;
 import net.minecraft.src.EntityPlayer;
-import net.minecraft.src.InventoryPlayer;
-import net.minecraft.src.ItemStack;
 import net.minecraft.src.World;
 
 public abstract class HammerAnvilHelper {
     public static boolean tryHammerHeldItem(World world, int x, int y, int z, EntityPlayer player, TileEntityHammerAnvil anvil) {
-        if (anvil != null && anvil.isBusy()) {
-            return false;
-        }
-        if (!player.capabilities.isCreativeMode && player.getFoodStats().getFoodLevel() < 6) {
-            sendStatus(player, anvil, "You are too hungry to use the anvil.");
-            return false;
-        }
-
-        ItemStack input = player.getHeldItem();
-        HammerRecipe recipe = HammerCraftingManager.instance.getRecipe(input);
-        if (recipe == null) {
-            sendStatus(player, anvil, "No hammer recipe for held item.");
-            return false;
-        }
-
-        int hammerSlot = findUsableHammerSlot(player, recipe);
-        if (hammerSlot < 0) {
-            sendStatus(player, anvil, "A valid hammer is required.");
-            return false;
-        }
-
-        int hits = recipe.getHitsRequired();
-        if (anvil != null && !anvil.canSpendHits(hits)) {
-            if (!anvil.isWaitingToBreak()) {
-                sendStatus(player, anvil, "The anvil is too worn for this recipe.");
-            }
-            return false;
-        }
-
-        ItemStack hammer = player.inventory.mainInventory[hammerSlot];
-        ItemStack recipeInput = recipe.getInput();
-        if (!player.capabilities.isCreativeMode) {
-            input.stackSize -= recipeInput.stackSize;
-            if (input.stackSize <= 0) {
-                player.inventory.mainInventory[player.inventory.currentItem] = null;
-            }
-
-            hammer.damageItem(hits, player);
-            if (hammer.stackSize <= 0) {
-                player.inventory.mainInventory[hammerSlot] = null;
-            }
-        }
-
-        recipe.chargePlayerExperience(player);
-        player.addExhaustion(0.2F * hits);
-
-        for (ItemStack output : recipe.getOutput()) {
-            if (output == null) {
-                continue;
-            }
-            ItemStack outputCopy = output.copy();
-            player.inventory.addItemStackToInventory(outputCopy);
-            if (outputCopy.stackSize > 0) {
-                spawnOutputAboveAnvil(world, x, y, z, outputCopy);
-            }
-        }
-
-        if (anvil != null) {
-            anvil.spendHits(hits);
-        } else {
-            world.playSoundEffect(x + 0.5D, y + 0.5D, z + 0.5D,
-                    "random.anvil_use", 0.5F, world.rand.nextFloat() * 0.25F + 1.25F);
-        }
-
-        player.inventory.onInventoryChanged();
-        return true;
-    }
-
-    private static void spawnOutputAboveAnvil(World world, int x, int y, int z, ItemStack stack) {
-        EntityItem entity = new EntityItem(world, x + 0.5D, y + 1.2D, z + 0.5D, stack);
-        entity.motionX = 0.0D;
-        entity.motionY = 0.0D;
-        entity.motionZ = 0.0D;
-        world.spawnEntityInWorld(entity);
-    }
-
-    private static int findUsableHammerSlot(EntityPlayer player, HammerRecipe recipe) {
-        InventoryPlayer inventory = player.inventory;
-        for (int i = 0; i < inventory.mainInventory.length; ++i) {
-            ItemStack stack = inventory.mainInventory[i];
-            if (stack != null && stack.getItem() instanceof ItemHammer && recipe.canPlayerUseHammer(stack, player)) {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    public static void sendStatus(EntityPlayer player, TileEntityHammerAnvil anvil, String message) {
-        if (player == null || player.worldObj.isRemote) {
-            return;
-        }
-        String suffix = "";
-        if (anvil != null && anvil.isLimited()) {
-            suffix = " Uses: " + anvil.getUsesRemaining() + " / " + anvil.getMaxUses() + ".";
-        }
-        player.sendChatToPlayer(new ChatMessageComponent().addText(message + suffix));
+        return anvil != null && anvil.tryStartHammerOperation(player);
     }
 }
