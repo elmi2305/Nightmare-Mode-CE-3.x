fab5f4915ef1fae65e1d1cc251fdb9831ed52f80
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-19 20:52:28 +0200

    added twig drop chance bonus

--- src/main/java/com/itlesports/nightmaremode/mixin/blocks/BlockLeavesMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/blocks/BlockLeavesMixin.java
@@ -2,6 +2,7 @@ package com.itlesports.nightmaremode.mixin.blocks;
 
 import btw.community.nightmaremode.NightmareMode;
 import com.itlesports.nightmaremode.item.NMItems;
+import com.itlesports.nightmaremode.skill.SkillHandler;
 import com.itlesports.nightmaremode.util.elements.NMEvents;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
@@ -34,6 +35,16 @@ public class BlockLeavesMixin extends BlockLeavesBase {
             return;
         }
         cir.setReturnValue(NMItems.leaf.itemID);
+    }
+
+    @Override
+    public void dropBlockAsItemWithChance(World world, int x, int y, int z, int metadata, float chance, int fortune) {
+        super.dropBlockAsItemWithChance(world, x, y, z, metadata, chance, fortune);
+        EntityPlayer player = world.getClosestPlayer(x + 0.5D, y + 0.5D, z + 0.5D, 8.0D);
+        float bonus = player == null ? 0.0F : SkillHandler.getPlayerData(player).twigDropChanceBonus;
+        if (!world.isRemote && bonus > 0.0F && world.rand.nextFloat() < bonus) {
+            this.dropBlockAsItem_do(world, x, y, z, new ItemStack(NMItems.twig));
+        }
     }
 
     @Override
