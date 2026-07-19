ff59190520c9effe5583686e70aaab1c1427996e
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-19 20:54:03 +0200

    plant fiber drop rate skill

--- src/main/java/com/itlesports/nightmaremode/mixin/blocks/BlockTallGrassMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/blocks/BlockTallGrassMixin.java
@@ -3,6 +3,7 @@ package com.itlesports.nightmaremode.mixin.blocks;
 import btw.community.nightmaremode.NightmareMode;
 import com.itlesports.nightmaremode.item.NMItems;
 import com.itlesports.nightmaremode.util.interfaces.EntityPlayerExt;
+import com.itlesports.nightmaremode.skill.SkillHandler;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
 import net.minecraft.src.*;
@@ -33,7 +34,10 @@ public class BlockTallGrassMixin extends BlockFlower {
 
     @Inject(method = "dropBlockAsItemWithChance", at = @At("HEAD"))
     private void dropPlantFiber(World world, int x, int y, int z, int metadata, float chance, int fortuneModifier, CallbackInfo ci) {
-        if (!world.isRemote && world.rand.nextFloat() <= 0.08f) {
+        EntityPlayer player = world.getClosestPlayer(x + 0.5D, y + 0.5D, z + 0.5D, 8.0D);
+        float bonus = player == null ? 0.0F : SkillHandler.getPlayerData(player).tallGrassPlantFiberChanceBonus;
+        boolean guaranteed = player != null && SkillHandler.getPlayerData(player).tallGrassAlwaysDropsPlantFiber;
+        if (!world.isRemote && (guaranteed || world.rand.nextFloat() <= 0.08F + bonus)) {
             this.dropBlockAsItem_do(world, x, y, z, new ItemStack(NMItems.plantFiber));
         }
     }
