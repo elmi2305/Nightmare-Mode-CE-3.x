48cbaf51f6f9a4d9d2b911a4cc9ca878b06b45b7
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-19 21:03:45 +0200

    added hammer recipe for sharp stone

--- src/main/java/com/itlesports/nightmaremode/crafting/recipe/HammerRecipeList.java
+++ src/main/java/com/itlesports/nightmaremode/crafting/recipe/HammerRecipeList.java
@@ -1,5 +1,6 @@
 package com.itlesports.nightmaremode.crafting.recipe;
 
+import btw.block.BTWBlocks;
 import btw.item.BTWItems;
 import com.itlesports.nightmaremode.block.NMBlocks;
 import com.itlesports.nightmaremode.crafting.manager.HammerCraftingManager;
@@ -11,6 +12,7 @@ public abstract class HammerRecipeList {
     public static void addRecipes() {
 
         HammerCraftingManager.instance.addRecipe(new ItemStack(NMItems.ovenPart, 1), Block.stoneSingleSlab, new int[]{4, 12}).setCanBeMinedByAnyHammer();
+        HammerCraftingManager.instance.addRecipe(new ItemStack(BTWItems.sharpStone), BTWBlocks.looseCobblestoneSlab, new int[]{0, 4, 8}).setCanBeMinedByAnyHammer();
         HammerCraftingManager.instance.addRecipe(new ItemStack(BTWItems.ironNugget, 1), NMBlocks.ironBloom, new int[]{0, 1, 2, 3, 4, 5, 6, 7}).setHitsRequired(8);
 
         HammerCraftingManager.instance.addItemRecipe(new ItemStack(NMItems.ovenPart, 1), new ItemStack(Block.stoneSingleSlab, 1, 4));
