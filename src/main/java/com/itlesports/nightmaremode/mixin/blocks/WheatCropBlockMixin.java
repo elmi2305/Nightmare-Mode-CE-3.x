91e91c2b6509a2f777201bb967f47f44d594fbc5
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-28 08:40:36 +0200

    wheat can only be harvested with scythes

--- src/main/java/com/itlesports/nightmaremode/mixin/blocks/WheatCropBlockMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/blocks/WheatCropBlockMixin.java
@@ -1,8 +1,12 @@
 package com.itlesports.nightmaremode.mixin.blocks;
 
+import api.block.blocks.DailyGrowthCropsBlock;
 import btw.block.blocks.WheatCropBlock;
 import com.itlesports.nightmaremode.agriculture.ChunkAttributeManager;
+import com.itlesports.nightmaremode.item.items.ItemScythe;
 import net.minecraft.src.Block;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.ItemStack;
 import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -11,7 +15,16 @@ import org.spongepowered.asm.mixin.injection.Redirect;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(WheatCropBlock.class)
-public class WheatCropBlockMixin {
+public abstract class  WheatCropBlockMixin extends DailyGrowthCropsBlock {
+    protected WheatCropBlockMixin(int iBlockID) {
+        super(iBlockID);
+    }
+
+    public float getPlayerRelativeBlockHardness(EntityPlayer player, World world, int x, int y, int z) {
+        ItemStack heldStack = player.getCurrentEquippedItem();
+        return heldStack != null && heldStack.getItem() instanceof ItemScythe ? 10.0F : 0.0F;
+    }
+
     @Inject(method = "incrementGrowthLevel", at = @At("HEAD"), cancellable = true)
     private void requireChunkResources(World world, int x, int y, int z, CallbackInfo ci) {
         if (!ChunkAttributeManager.canGrow(world, x, z, (Block)(Object)this)) {
--- src/main/java/com/itlesports/nightmaremode/mixin/blocks/WheatCropTopBlockMixin.java
+++ src/main/java/com/itlesports/nightmaremode/mixin/blocks/WheatCropTopBlockMixin.java
@@ -1,15 +1,37 @@
 package com.itlesports.nightmaremode.mixin.blocks;
 
+import api.block.blocks.DailyGrowthCropsBlock;
 import btw.block.blocks.WheatCropTopBlock;
 import com.itlesports.nightmaremode.agriculture.ChunkAttributeManager;
+import com.itlesports.nightmaremode.item.items.ItemScythe;
 import net.minecraft.src.Block;
+import net.minecraft.src.EntityPlayer;
+import net.minecraft.src.ItemStack;
 import net.minecraft.src.World;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(WheatCropTopBlock.class)
-public class WheatCropTopBlockMixin {
+public abstract class WheatCropTopBlockMixin extends DailyGrowthCropsBlock {
+    protected WheatCropTopBlockMixin(int iBlockID) {
+        super(iBlockID);
+    }
+
+    public float getPlayerRelativeBlockHardness(EntityPlayer player, World world, int x, int y, int z) {
+        ItemStack heldStack = player.getCurrentEquippedItem();
+        return heldStack != null && heldStack.getItem() instanceof ItemScythe ? 10.0F : 0.0F;
+    }
+
+
+    /** Wheat is harvested with a scythe, so saws cannot collect it. */
+    @Inject(method = "doesBlockDropAsItemOnSaw", at = @At("HEAD"), cancellable = true)
+    private void preventSawHarvest(World world, int x, int y, int z, CallbackInfoReturnable<Boolean> cir) {
+        cir.setReturnValue(false);
+    }
+
     @Redirect(
             method = "updateFlagForGrownToday",
             at = @At(
