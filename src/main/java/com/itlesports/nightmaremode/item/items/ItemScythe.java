3f9f99a6eec93d0c6d2e30191b83c84aa362a423
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-28 07:58:16 +0200

    fixed scythe placing as block

--- src/main/java/com/itlesports/nightmaremode/item/items/ItemScythe.java
+++ src/main/java/com/itlesports/nightmaremode/item/items/ItemScythe.java
@@ -1,6 +1,13 @@
 package com.itlesports.nightmaremode.item.items;
 
+import api.achievement.AchievementEventDispatcher;
 import api.item.items.SwordItem;
+import api.util.MiscUtils;
+import api.world.BlockPos;
+import api.world.WorldUtils;
+import btw.achievement.BTWAchievementEvents;
+import btw.block.BTWBlocks;
+import btw.block.tileentity.PlacedToolTileEntity;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
 import net.minecraft.src.*;
@@ -35,4 +42,23 @@ public class ItemScythe extends SwordItem {
     public EnumAction getItemUseAction(ItemStack par1ItemStack) {
         return EnumAction.none;
     }
+
+    @Override
+    public ItemStack onItemRightClick(ItemStack par1ItemStack, World par2World, EntityPlayer par3EntityPlayer) {
+        return par1ItemStack;
+    }
+
+    @Override
+    public boolean onItemUse(ItemStack stack, EntityPlayer player, World world, int i, int j, int k, int iFacing, float fClickX, float fClickY, float fClickZ) {
+        return false;
+    }
+
+    protected boolean canToolStickInBlock(ItemStack stack, Block block, World world, int i, int j, int k) {
+        return false;
+    }
+
+    protected boolean getCanBePlacedAsBlock() {
+        return false;
+    }
+
 }
