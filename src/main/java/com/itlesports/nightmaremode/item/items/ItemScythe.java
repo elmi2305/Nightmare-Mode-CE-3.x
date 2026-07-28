89898daef935918739c95581b3645bb69254f89c
Elmi <elmir.junuzovic7blm@gmail.com>
2026-07-28 07:42:36 +0200

    cannot block with scythe

--- src/main/java/com/itlesports/nightmaremode/item/items/ItemScythe.java
+++ src/main/java/com/itlesports/nightmaremode/item/items/ItemScythe.java
@@ -3,12 +3,7 @@ package com.itlesports.nightmaremode.item.items;
 import api.item.items.SwordItem;
 import com.google.common.collect.HashMultimap;
 import com.google.common.collect.Multimap;
-import net.minecraft.src.AttributeModifier;
-import net.minecraft.src.Enchantment;
-import net.minecraft.src.EnumToolMaterial;
-import net.minecraft.src.Item;
-import net.minecraft.src.ItemStack;
-import net.minecraft.src.SharedMonsterAttributes;
+import net.minecraft.src.*;
 
 public class ItemScythe extends SwordItem {
     private final float weaponDamage;
@@ -34,5 +29,10 @@ public class ItemScythe extends SwordItem {
                 new AttributeModifier(Item.field_111210_e, "Weapon modifier", this.weaponDamage, 0)
         );
         return modifiers;
+    }
+
+    @Override
+    public EnumAction getItemUseAction(ItemStack par1ItemStack) {
+        return EnumAction.none;
     }
 }
