a3112df5fff76b7470be00fb7f267d8787df56eb
Shiro <nltimotei72004@gmail.com>
2026-08-17 18:04:30 +0300

    Fixed tier 3 island and sky island structures

--- src/main/java/com/itlesports/nightmaremode/structure/SkyZiggurath.java
+++ src/main/java/com/itlesports/nightmaremode/structure/SkyZiggurath.java
@@ -17,6 +17,8 @@ public class SkyZiggurath extends NMStructure {
 
     public SkyZiggurath(Random random, int x, int z) {
         super(random, x, 200, z, 54, 30, 54);
+        // Keep the NBT template aligned with the world's positive X/Z axes.
+        this.coordBaseMode = 0;
         this.shouldGenerateAir = true;
     }
 
@@ -35,26 +37,38 @@ public class SkyZiggurath extends NMStructure {
 
     // Kept in lockstep with Tier3VillagerPost's palette, including its state indexes.
     private static PaletteEntry[] createPalette() {
-        PaletteEntry[] entries = new PaletteEntry[21];
+        PaletteEntry[] entries = new PaletteEntry[31];
         entries[0] = block(0, 0);
-        entries[1] = block(0, 0);
-        entries[2] = block(Block.blockNetherQuartz.blockID, 0);
-        entries[3] = block(Block.netherFence.blockID, 0);
-        entries[4] = block(Block.netherBrick.blockID, 0);
-        entries[5] = block(Block.glowStone.blockID, 0);
-        entries[6] = block(BTWBlocks.quartzMouldingAndDecorative.blockID, 12);
-        entries[7] = block(Block.obsidian.blockID, 0);
-        entries[8] = block(Block.endPortalFrame.blockID, 0);
-        entries[9] = block(Block.stoneSingleSlab.blockID, 0);
-        entries[10] = block(Block.netherrack.blockID, 0);
-        entries[11] = block(Block.enchantmentTable.blockID, 0);
-        entries[12] = block(Block.beacon.blockID, 0);
-        entries[13] = block(Block.oreDiamond.blockID, 0);
-        entries[14] = block(Block.wood.blockID, 0);
-        entries[15] = block(Block.chest.blockID, 0);
-        entries[16] = block(Block.oreCoal.blockID, 0);
-        entries[17] = block(Block.hay.blockID, 0);
-        entries[18] = block(Block.oreEmerald.blockID, 0);
+        entries[1] = block(Block.dirt.blockID, 0);
+        entries[2] = block(Block.stone.blockID, 0);
+        entries[3] = block(Block.stoneBrick.blockID, 8);
+        entries[4] = block(Block.brick.blockID, 0);
+        entries[5] = block(BTWBlocks.stoneBrickSlab.blockID, 2);
+        entries[6] = block(BTWBlocks.stoneBrickSlab.blockID, 10);
+        entries[7] = block(Block.skull.blockID, 4);
+        entries[8] = block(Block.web.blockID, 0);
+        entries[9] = block(Block.redstoneWire.blockID, 0);
+        entries[10] = block(BTWBlocks.chest.blockID, 3);
+        entries[11] = block(BTWBlocks.chest.blockID, 4);
+        entries[12] = block(Block.stoneBrick.blockID, 10);
+        entries[13] = block(BTWBlocks.chest.blockID, 4);
+        entries[14] = block(Block.netherrack.blockID, 0);
+        entries[15] = block(Block.fire.blockID, 0);
+        entries[16] = block(BTWBlocks.deepStrataStoneBrickStairs.blockID, 1);
+        entries[17] = block(BTWBlocks.deepStrataStoneBrickStairs.blockID, 0);
+        entries[18] = block(BTWBlocks.deepStrataStoneBrickStairs.blockID, 3);
+        entries[19] = block(BTWBlocks.deepStrataStoneBrickStairs.blockID, 7);
+        entries[20] = block(Block.fenceIron.blockID, 0);
+        entries[21] = block(Block.anvil.blockID, 0);
+        entries[22] = block(Block.skull.blockID, 1);
+        entries[23] = block(Block.obsidian.blockID, 0);
+        entries[24] = block(BTWBlocks.deepStrataStoneBrickStairs.blockID, 2);
+        entries[25] = block(BTWBlocks.deepStrataStoneBrickStairs.blockID, 4);
+        entries[26] = block(BTWBlocks.chest.blockID, 2);
+        entries[27] = block(BTWBlocks.chest.blockID, 5);
+        entries[28] = block(BTWBlocks.chest.blockID, 2);
+        entries[29] = block(Block.oreDiamond.blockID, 0);
+        entries[30] = block(Block.oreGold.blockID, 0);
         return entries;
     }
 }
--- src/main/java/com/itlesports/nightmaremode/structure/Tier3VillagerPost.java
+++ src/main/java/com/itlesports/nightmaremode/structure/Tier3VillagerPost.java
@@ -1,6 +1,7 @@
 package com.itlesports.nightmaremode.structure;
 
 import btw.block.BTWBlocks;
+import com.itlesports.nightmaremode.block.NMBlocks;
 import net.minecraft.src.Block;
 
 import java.util.Random;
@@ -30,21 +31,21 @@ public class Tier3VillagerPost extends NetherVillagerPost {
     }
     private static PaletteEntry[] createPalette() {
         tierThreeBlocks = new PaletteEntry[21];
-        tierThreeBlocks[0] = block(Block.netherrack.blockID, 4);
-        tierThreeBlocks[1] = block(0, 0);
-        tierThreeBlocks[2] = block(Block.blockNetherQuartz.blockID, 0);
-        tierThreeBlocks[3] = block(Block.netherFence.blockID, 0);
-        tierThreeBlocks[4] = block(Block.netherBrick.blockID, 0);
-        tierThreeBlocks[5] = block(Block.glowStone.blockID, 0);
-        tierThreeBlocks[6] = block(BTWBlocks.quartzMouldingAndDecorative.blockID, 12); // quartz pillar
-        tierThreeBlocks[7] = block(Block.obsidian.blockID, 0);
-        tierThreeBlocks[8] = block(Block.endPortalFrame.blockID, 0);
-        tierThreeBlocks[9] = block(Block.stoneSingleSlab.blockID, 0);
-        tierThreeBlocks[10] = block(Block.netherrack.blockID, 0);
+        tierThreeBlocks[0] = block(0, 0);
+        tierThreeBlocks[1] = block(Block.netherrack.blockID, 4);
+        tierThreeBlocks[2] = block(Block.obsidian.blockID, 0);
+        tierThreeBlocks[3] = block(Block.netherBrick.blockID, 0);
+        tierThreeBlocks[4] = block(Block.glowStone.blockID, 0);
+        tierThreeBlocks[5] = block(Block.netherrack.blockID, 0);
+        tierThreeBlocks[6] = block(Block.fire.blockID, 0);
+        tierThreeBlocks[7] = block(Block.blockNetherQuartz.blockID, 1);
+        tierThreeBlocks[8] = block(Block.netherFence.blockID, 0);
+        tierThreeBlocks[9] = block(Block.stoneSingleSlab.blockID, 14);
+        tierThreeBlocks[10] = block(NMBlocks.steelOre.blockID, 0);
         tierThreeBlocks[11] = block(Block.enchantmentTable.blockID, 0);
-        tierThreeBlocks[12] = block(Block.beacon.blockID, 0);
-        tierThreeBlocks[13] = block(Block.oreDiamond.blockID, 0);
-        tierThreeBlocks[14] = block(Block.wood.blockID, 0);
+        tierThreeBlocks[12] = block(BTWBlocks.quartzMouldingAndDecorative.blockID, 12);
+        tierThreeBlocks[13] = block(121, 0);
+        tierThreeBlocks[14] = block(Block.endPortalFrame.blockID, 0);
         tierThreeBlocks[15] = block(Block.chest.blockID, 0);
         tierThreeBlocks[16] = block(Block.oreCoal.blockID, 0);
         tierThreeBlocks[17] = block(Block.hay.blockID, 0);
