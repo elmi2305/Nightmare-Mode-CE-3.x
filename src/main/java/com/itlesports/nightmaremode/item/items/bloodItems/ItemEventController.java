09cb972da8ac9f534feb5986d78765a082336710
Elmi <elmir.junuzovic7blm@gmail.com>
2026-08-22 21:30:33 +0200

    synchronize blood moon activation

--- src/main/java/com/itlesports/nightmaremode/item/items/bloodItems/ItemEventController.java
+++ src/main/java/com/itlesports/nightmaremode/item/items/bloodItems/ItemEventController.java
@@ -5,13 +5,20 @@ import com.itlesports.nightmaremode.item.items.template.ItemAchievementGranter;
 import net.minecraft.server.MinecraftServer;
 import net.minecraft.src.*;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.UUID;
 
 public class ItemEventController extends ItemAchievementGranter {
     private final int eventType;
 
     public static int EVENT_BLOODMOON = 1;
     public static int EVENT_ECLIPSE  = 2;
+    private static final long EVENT_DRINK_WINDOW_TICKS = 300L;
+    private static final Map<UUID, Long> recentBloodMoonDrinkers = new HashMap<>();
     public ItemEventController(int iItemID, int type, Achievement... achievements) {
         super(iItemID, achievements);
         this.eventType = type;
@@ -20,20 +27,54 @@ public class ItemEventController extends ItemAchievementGranter {
 
     @Override
     public ItemStack onEaten(ItemStack stack, World w, EntityPlayer player) {
-        if(this.eventType != 0){
-            performEvent(eventType, w);
+        if (!w.isRemote && this.eventType != 0) {
+            if (this.eventType != EVENT_BLOODMOON || allOnlinePlayersDrankBlood(player, w)) {
+                performEvent(eventType, w);
+            }
         }
 
 
         return super.onEaten(stack, w, player);
     }
+
+    private boolean allOnlinePlayersDrankBlood(EntityPlayer drinkingPlayer, World world) {
+        MinecraftServer server = MinecraftServer.getServer();
+        if (server == null) {
+            return true;
+        }
+
+        long now = world.getTotalWorldTime();
+        Set<UUID> onlinePlayers = new HashSet<>();
+        for (Object obj : server.getConfigurationManager().playerEntityList) {
+            if (obj instanceof EntityPlayer player) {
+                onlinePlayers.add(player.getUniqueID());
+            }
+        }
+
+        recentBloodMoonDrinkers.keySet().retainAll(onlinePlayers);
+        recentBloodMoonDrinkers.entrySet().removeIf(entry -> now - entry.getValue() > EVENT_DRINK_WINDOW_TICKS);
+        recentBloodMoonDrinkers.put(drinkingPlayer.getUniqueID(), now);
+
+        if (!recentBloodMoonDrinkers.keySet().containsAll(onlinePlayers)) {
+            return false;
+        }
+
+        recentBloodMoonDrinkers.clear();
+        return true;
+    }
+
     private void performEvent(int type, World world){
+        MinecraftServer server = MinecraftServer.getServer();
+        if (server != null) {
+            world = server.worldServers[0];
+        }
+
         if(type == EVENT_BLOODMOON){
             long time = world.getWorldTime();
 
             if(NMUtils.getIsBloodMoon()){
                 time = (long)Math.floor(((double) (time + 24000) / 24000)) * 24000L;
-            } else if (!MinecraftServer.getIsServer()) {
+            } else {
                 time = NMUtils.getNextBloodMoonTime(time);
             }
             world.setWorldTime(time);
@@ -43,7 +84,7 @@ public class ItemEventController extends ItemAchievementGranter {
 
             if(NMUtils.getIsEclipse()){
                 time = (long)Math.floor(((double) (time + 24000) / 24000)) * 24000L;
-            } else if (!MinecraftServer.getIsServer()) {
+            } else {
                 time = NMUtils.getNextEclipseTime(time);
             }
             world.setWorldTime(time);
