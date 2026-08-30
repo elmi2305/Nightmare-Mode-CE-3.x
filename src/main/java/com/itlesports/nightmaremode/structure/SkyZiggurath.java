3d5292fa8d24bc5fcdbe54194bbfd206ec8708c2
elmi <elmir.junuzovic7blm@gmail.com>
2026-08-30 14:31:07 +0200

    added mob logic to ziggurat

--- src/main/java/com/itlesports/nightmaremode/structure/SkyZiggurath.java
+++ src/main/java/com/itlesports/nightmaremode/structure/SkyZiggurath.java
@@ -1,15 +1,33 @@
 package com.itlesports.nightmaremode.structure;
 
 import btw.block.BTWBlocks;
+import btw.item.BTWItems;
+import api.world.BlockPos;
+import com.itlesports.nightmaremode.block.NMBlocks;
+import com.itlesports.nightmaremode.item.NMItems;
+import com.itlesports.nightmaremode.item.NMPostItems;
+import com.itlesports.nightmaremode.underworld.poi.LootEntry;
 import com.itlesports.nightmaremode.underworld.poi.scatteredfeatures.utils.NMStructure;
 import net.minecraft.src.Block;
+import net.minecraft.src.Item;
+import net.minecraft.src.ItemStack;
+import net.minecraft.src.TileEntityMobSpawner;
+import net.minecraft.src.World;
 
+import java.util.Arrays;
+import java.util.List;
 import java.util.Random;
 
 /** A 54 x 54 sky landmark placed from structures/skyZiggurath.nbt at Y=200. */
 public class SkyZiggurath extends NMStructure {
     public static final int MIN_CHUNKS_APART = 32;
     public static final int MAX_CHUNKS_APART = 48;
+    private static final String[] SPAWNER_MOBS = {
+            "NmSkyZiggurathWitherSkeleton",
+            "NmSkyZiggurathJungleSpider",
+            "NmSkyZiggurathDeadzonePigman",
+            "NmSkyZiggurathShadowZombie"
+    };
     private static PaletteEntry[] palette;
 
     public SkyZiggurath() {
@@ -42,16 +60,16 @@ public class SkyZiggurath extends NMStructure {
         entries[1] = block(Block.dirt.blockID, 0);
         entries[2] = block(Block.stone.blockID, 0);
         entries[3] = block(Block.stoneBrick.blockID, 8);
-        entries[4] = block(Block.brick.blockID, 0);
+        entries[4] = block(Block.mobSpawner.blockID, 0);
         entries[5] = block(BTWBlocks.stoneBrickSlab.blockID, 2);
         entries[6] = block(BTWBlocks.stoneBrickSlab.blockID, 10);
         entries[7] = block(Block.skull.blockID, 4);
         entries[8] = block(Block.web.blockID, 0);
         entries[9] = block(Block.redstoneWire.blockID, 0);
-        entries[10] = block(BTWBlocks.chest.blockID, 3);
-        entries[11] = block(BTWBlocks.chest.blockID, 4);
+        entries[10] = lootContainer(3);
+        entries[11] = lootContainer(4);
         entries[12] = block(Block.stoneBrick.blockID, 10);
-        entries[13] = block(BTWBlocks.chest.blockID, 4);
+        entries[13] = lootContainer(4);
         entries[14] = block(Block.netherrack.blockID, 0);
         entries[15] = block(Block.fire.blockID, 0);
         entries[16] = block(BTWBlocks.deepStrataStoneBrickStairs.blockID, 1);
@@ -64,11 +82,48 @@ public class SkyZiggurath extends NMStructure {
         entries[23] = block(Block.obsidian.blockID, 0);
         entries[24] = block(BTWBlocks.deepStrataStoneBrickStairs.blockID, 2);
         entries[25] = block(BTWBlocks.deepStrataStoneBrickStairs.blockID, 4);
-        entries[26] = block(BTWBlocks.chest.blockID, 2);
-        entries[27] = block(BTWBlocks.chest.blockID, 5);
-        entries[28] = block(BTWBlocks.chest.blockID, 2);
+        entries[26] = lootContainer(2);
+        entries[27] = lootContainer(5);
+        entries[28] = lootContainer(2);
         entries[29] = block(Block.oreDiamond.blockID, 0);
         entries[30] = block(Block.oreGold.blockID, 0);
         return entries;
+    }
+
+    @Override
+    protected int getLootContainerBlockID() {
+        return NMBlocks.bloodChest.blockID;
+    }
+
+    @Override
+    protected int getLootRollCount(BlockPos position, Random random) {
+        return random.nextInt(5) + 5;
+    }
+
+    @Override
+    protected void configureSpawner(World world, BlockPos position, TileEntityMobSpawner spawner, Random random) {
+        spawner.getSpawnerLogic().setMobID(SPAWNER_MOBS[random.nextInt(SPAWNER_MOBS.length)]);
+    }
+
+    @Override
+    protected List<LootEntry> getLootPool() {
+        return Arrays.asList(
+                // Common supplies: make the expedition materially worthwhile without trivializing it.
+                new LootEntry(new ItemStack(BTWItems.steelNugget), 24, 4, 12),
+                new LootEntry(new ItemStack(BTWItems.soulforgedSteelIngot), 10, 1, 3),
+                new LootEntry(new ItemStack(NMItems.bloodOrb), 16, 2, 6),
+                new LootEntry(new ItemStack(NMItems.bloodIngot), 10, 1, 2),
+                new LootEntry(new ItemStack(Item.appleGold), 10, 1, 2),
+
+                // The sky is a post-Wither reward source for the materials used to reach it.
+                new LootEntry(new ItemStack(NMItems.deadzoneShard), 16, 4, 10),
+                new LootEntry(new ItemStack(NMItems.coresteelIngot), 7, 1, 2),
+                new LootEntry(new ItemStack(NMItems.deadzoneAlloyIngot), 6, 1, 2),
+                new LootEntry(new ItemStack(NMItems.deadzoneAlloyPlate), 3, 1, 1),
+
+                // Jackpot rolls stay scarce across the structure's several chests.
+                new LootEntry(new ItemStack(Item.appleGold, 1, 1), 1, 1, 1),
+                new LootEntry(new ItemStack(NMPostItems.bloodMoonBottle), 2, 1, 1)
+        );
     }
 }
